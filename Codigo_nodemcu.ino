bb707ae6d12ff752a880423542ff6572418f3dd7
LucasMgNo <103533464+LucasMgNo@users.noreply.github.com>
2023-03-22 12:39:03 -0300

    Update Codigo_nodemcu.ino

--- Codigo_nodemcu.ino
+++ Codigo_nodemcu.ino
@@ -1,14 +1,14 @@
 #include <SoftwareSerial.h>
-#include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>        //biblioteca wifi do nodemcu
+#include <FirebaseArduino.h>    //biblioteca para o nodemcu conectar dom o firebase
 
 // Set these to run example.
-#define FIREBASE_HOST "horta-monitorada-default-rtdb.firebaseio.com"
-#define FIREBASE_AUTH "VF02UvmYYFHxikxGghYeL6i0W8KPPxDXjPI0wCi2"
-#define WIFI_SSID "TddsFibra_Nem Tenta "
+#define FIREBASE_HOST "horta-monitorada-default-rtdb.firebaseio.com"      //endereco do banco de dados
+#define FIREBASE_AUTH "VF02UvmYYFHxikxGghYeL6i0W8KPPxDXjPI0wCi2"          //codigo de seguranca do banco
+#define WIFI_SSID "TddsFibra_Nem Tenta "                                  //informacoes do wifi
 #define WIFI_PASSWORD "tempos2525"
 
-SoftwareSerial mySerial (D1, D2);
+SoftwareSerial mySerial (D1, D2);             //configurar as portas do nodemcu para funcionar com RX e TX
 
 
 void setup(){
@@ -23,17 +23,18 @@ void setup(){
     delay(500);
   }
   Serial.println(WiFi.localIP());
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);    //inicia a conexao com o banco de dados
 }
 
-int humidAr, tempAr, temp1, temp2,humid1;
+int humidAr, tempAr, temp1, temp2,humid1;    //variaveis globais das informacoes recebbidas dos sensores
 
 void loop(){
-  if (mySerial.available()){
-  String recebido = mySerial.readStringUntil('\n');
-  String msg = recebido;
-
-  if(msg.startsWith("T")){
+  if (mySerial.available()){                //verifica se tem dados na porta serial
+  String recebido = mySerial.readStringUntil('\n');       //recebe os dados da porta serial
+  String msg = recebido;                    //atribui o dado recebido a uma string para ser feita a manipulacao nessa string
+  
+                              //a partir daqui sao feitas a logica para distinguir de qual sensor é a informação recebida, atraves de manipulação de string    
+  if(msg.startsWith("T")){    //verifica a primeira letra de identificacao da variavel recebida para atribuir a sua respectiva variavel
     msg.remove(0,1);
     tempAr = msg.toInt();
   } 
@@ -51,7 +52,7 @@ void loop(){
   }
   }
 
-  Serial.print(humidAr);
+  Serial.print(humidAr);        //imprime as informacoes para visualizacao no console
   Serial.print("\n");
   Serial.print(tempAr);
   Serial.print("\n");
@@ -61,7 +62,7 @@ void loop(){
   Serial.print("\n");
   delay(1000);
 
-  Firebase.setInt("ar/temperatura",tempAr);
+  Firebase.setInt("ar/temperatura",tempAr);           //envia as variaveis recebidas pelo serial para o firebase
   Firebase.setInt("ar/humidade",humidAr);
   Firebase.setInt("solo1/temperatura",temp1);
   Firebase.setInt("solo2/temperatura",temp2);
